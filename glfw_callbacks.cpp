197d55fee170bf6664f0437489486ed33a5c49b9
austinburnett <aburn1011@gmail.com>
2024-09-23 13:49:20 -0400

    Remove glfw callback implementations from main

---
+++ glfw_callbacks.cpp
@@ -0,0 +1,63 @@
+// This module contains implementations for any glfw callbacks that are set in main.cpp
+// as well as any implementation for any related functions.
+
+#include <iostream>
+#include <thread>
+
+#include <OpenGL/gl.h>
+// Ensure GLFW doesn't include the OpenGL header
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+// Flag that dictates whether or not the worker thread will plot squares
+static bool READY = false;
+
+static std::thread worker;
+
+// GLFW callback declarations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+void error_callback(int error, const char* description);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+// Utility function declarations
+void plot_particles_on_screen(double xpos, double ypos, GLFWwindow* window);
+
+// Whenever the window size changes this callback function executes
+inline void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+// Process all input every frame
+inline void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+// Whenever an error occurs this callback function executes
+inline void error_callback(int error, const char* description) {
+    std::cout << "Error: " << error << '\n' << description << '\n';
+}
+
+// Whenever a mouse button is pressed this callback function executes to
+// append a new position at the location where the left mouse button was pressed
+// so a primitive can be rendered at said position.
+inline void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+    double xpos, ypos;
+    int width, height;
+    static bool has_started = 0;
+
+    if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        READY = true;
+        if(!has_started) {
+            has_started = 1;
+            worker = std::thread(plot_particles_on_screen, xpos, ypos, window);
+        }
+    }
+    else if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+        READY = false;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -10,32 +10,25 @@
 #include <glad/glad.h>
 #endif
 
+#include <iostream>
+#include <thread>
+
 #include "GLFW/glfw3.h"
 #include <glm/glm.hpp>
 #include <glm/ext/matrix_transform.hpp>
-#include <iostream>
-#include <thread>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #include "shader.hpp"
+#include "glfw_callbacks.cpp"
 #include "includes/square.hpp"
 #include "includes/particle.hpp"
 
-void plot_particles_on_screen(double xpos, double ypos, GLFWwindow* window);
 glm::vec3 screen_to_ndc(double x, double y, const int width, const int height);
 glm::vec3 matrix_to_ndc(int i, int j, const int width, const int height);
 bool is_particle_in_row(int row);
 void update_particles_on_screen(int start, int end, Shader& ourShader);
-
-// GLFW callbacks
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow *window);
-void error_callback(int error, const char* description);
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 
 // Settings
 const unsigned int SCR_WIDTH = 550;
@@ -46,12 +39,12 @@ float DELTA_TIME = 0.0f; // Time between current and last frame
 float LAST_FRAME = 0.0f; // Time of last frame
 
 // Flag that dictates whether or not the worker thread will plot squares
-static bool READY = false;
+extern bool READY;
 
 // Flag that terminates the worker thread's while loop
 static bool IS_RUNNING = true;
 
-static std::thread worker;
+extern std::thread worker;
 
 // Pixel Simulation Buffer
 // [0][0] is the bottom-left corner of the window
@@ -59,7 +52,6 @@ Particle* grid[SCR_WIDTH][SCR_HEIGHT];
 
 int main() {
     // Initialize and configure glfw
-    glfwSetErrorCallback(error_callback);
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -77,9 +69,10 @@ int main() {
         return -1;
     }
     glfwMakeContextCurrent(window);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
     // Set glfw callbacks
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    glfwSetErrorCallback(error_callback);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetMouseButtonCallback(window, mouse_button_callback);
 
@@ -159,23 +152,6 @@ int main() {
     return (EXIT_SUCCESS);
 }
 
-// Process all input every frame
-void processInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
-}
-
-// Whenever the window size changes this callback function executes
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
-// Whenever an error occurs this callback function executes
-void error_callback(int error, const char* description) {
-    std::cout << "Error: " << error << '\n' << description << '\n';
-}
-
 // Plots the particle on the screen at the cursor's location
 void plot_particles_on_screen(double xpos, double ypos, GLFWwindow* window) {
     while(IS_RUNNING) {
@@ -185,26 +161,6 @@ void plot_particles_on_screen(double xpos, double ypos, GLFWwindow* window) {
                 if(int(xpos) < SCR_WIDTH && int(ypos) < SCR_HEIGHT)
                     grid[int(xpos)][int(SCR_HEIGHT-ypos)] = new WaterParticle();
         }
-    }
-}
-
-// Whenever a mouse button is pressed this callback function executes to
-// append a new position at the location where the left mouse button was pressed
-// so a primitive can be rendered at said position.
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
-    double xpos, ypos;
-    int width, height;
-    static bool has_started = 0;
-
-    if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-        READY = true;
-        if(!has_started) {
-            has_started = 1;
-            worker = std::thread(plot_particles_on_screen, xpos, ypos, window);
-        }
-    }
-    else if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
-        READY = false;
     }
 }
 
